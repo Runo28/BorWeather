6203b7ac3e781a4905acf6ea858c90d24b72d46e
Runo28 <runo283@gmail.com>
2021-05-02 01:21:03 +0300

    add new line

--- src/main/java/bot/telegram/Weather/weatherOTG.java
+++ src/main/java/bot/telegram/Weather/weatherOTG.java
@@ -15,6 +15,7 @@ public class weatherOTG {
         String urlPeremoha = "https://ua.sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%BF%D0%B5%D1%80%D0%B5%D0%BC%D0%BE%D0%B3%D0%B0-303020238";
         String urlDmutpivka = "https://ua.sinoptik.ua/%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0-%D0%B4%D0%BC%D0%B8%D1%82%D1%80%D1%96%D0%B2%D0%BA%D0%B0-303007087";
 
+
         if (local==1){
             return Jsoup.parse(new URL(urlPidgorodne),5000 );
         }
